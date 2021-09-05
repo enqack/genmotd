a2343632ad44be5c876c007dd064870025c54ade
enqack <enqack@gmail.com>
2021-09-05 10:12:16 -0400

    Version bump from 0.1.0 to 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='genmotd',
-    version='0.1.0',
+    version='0.2.0',
     description='Generates a Message of the Day',
     long_description=readme,
     classifiers=[
